f9a7e74560ae218fde8c0ddf70aeb6087e9b8e5a
angele-becomtech <a.henry@becomtech.fr>
2025-07-30 10:18:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function couleurAleatoire() {
     }
     return couleur;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var texteSuiveur = document.getElementById('blocSuiveur');
+
+    document.addEventListener('mousemove', function(e) {
+        // Met à jour la position du texte pour suivre la souris
+        texteSuiveur.style.left = (e.pageX + 10) + 'px';
+        texteSuiveur.style.top = (e.pageY + 10) + 'px';
+    });
+});
+
